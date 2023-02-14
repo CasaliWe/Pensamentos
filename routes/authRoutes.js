675a7737fe62c8c50c6aea86c69df8e9ce9c0e7c
Weslei Casali <wesleicasali18@gmail.com>
2023-02-14 20:27:14 -0300

    att

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -1,6 +1,6 @@
 const express = require('express')
 const router = express.Router()
-const AuthController = require("../controllers/authController")
+const AuthController = require("../controllers/AuthController")
 
 
 router.get('/login', AuthController.login)
